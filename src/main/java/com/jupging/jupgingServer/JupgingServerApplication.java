4e3df9e42784266e3816257a8bbfbfe93784684b
HONGMINJU <mhong0800@gmail.com>
2022-01-27 18:40:34 +0900

    :tada: 프로젝트 기본 구조 설정

---
+++ src/main/java/com/jupging/jupgingServer/JupgingServerApplication.java
@@ -0,0 +1,13 @@
+package com.jupging.jupgingServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JupgingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JupgingServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jupging/jupgingServer/JupgingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jupging.jupgingServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JupgingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
