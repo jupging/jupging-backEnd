fcab3458d597fb957ceb566e94edbd3fa429b60e
HONGMINJU <mhong0800@gmail.com>
2022-02-03 15:56:30 +0900

    feat : trashcan entity 추가

--- src/main/java/com/jupging/jupgingServer/shop/domain/Shop.java
+++ src/main/java/com/jupging/jupgingServer/shop/domain/Shop.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
-@Getter @Setter
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
---
+++ src/main/java/com/jupging/jupgingServer/trashcan/domain/Trashcan.java
@@ -0,0 +1,24 @@
+package com.jupging.jupgingServer.trashcan.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Trashcan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Double latitude;
+
+    @Column(nullable = false)
+    private Double longitude;
+}
